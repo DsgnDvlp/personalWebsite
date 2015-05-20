347b8d57da9ffa7edd368f771e44283ad793eaa3
mik115 <michele.pini2@gmail.com>
2015-05-20 19:38:01 +0200

    Ottimizzazione parte mobile e creazione terza slide parte about

--- pages/about.php
+++ pages/about.php
@@ -45,7 +45,51 @@
             		</div>
             	</div>
             </div>
-            <div class="section second">Section 2</div>
+            <div class="section second">
+            	<div class="topPanel"><span class="text">back</span></div>
+            	<div class="centerLine"></div>
+            	<div class="centerBlock"><span class="text">what i do</span></div>
+            	
+            	<div class="contentBlockContainer left">
+            		<div class="contentBlockContent">
+            			<div class="contentBlock">
+            				<img src="../img/uxUi.svg"/>
+            				<p class="title">UX / UI</p>
+            				<p class="content">I work as a UX &amp; UI designer, trying to search every aspect of design in 
+            			memories lost.</p>
+            			</div>
+            			<div class="contentBlock">
+            				<p class="title">web</p>
+            				<p class="content">I work as a UX &amp; UI designer, trying to search every aspect of design in 
+            			memories lost.</p>
+            			</div>
+            		</div>
+            	</div>
+            	<div class="contentBlockContainer right">
+            		<div class="contentBlockContent">
+            			<div class="contentBlock">
+            				<img class="visual" src="../img/visual.svg" />
+            				<p class="title">visual</p>
+            				<p class="content">I work as a UX &amp; UI designer, trying to search every aspect of design in 
+            			memories lost.</p>
+            			</div>
+            			<div class="contentBlock">
+            				<img src="../img/graphic.svg" />
+            				<p class="title">graphic</p>
+            				<p class="content">I work as a UX &amp; UI designer, trying to search every aspect of design in 
+            			memories lost.</p>
+            			</div>
+            		</div>
+            	</div>
+            	<div class="footer">
+            		<div class="goDown"><span class="text">keep going down</span><br/>
+            		</div>
+            		<div class="breadCrumbs">
+						<span>what i do</span>
+						<span>my cv</span>
+            		</div>
+            	</div>
+            </div>
             <div class="section third">Section 3</div>
             <div class="section">Section 4</div>
         </div>
